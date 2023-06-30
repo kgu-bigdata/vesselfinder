d21acee4ceb618bcd59aa469df8f0e38bf049e1c
kgu-bigdata <sjpark@kgu-bigdata.com>
2023-06-30 12:40:43 +0900

    retuen

--- python/vesselfinder_api/example_distance.py
+++ python/vesselfinder_api/example_distance.py
@@ -5,6 +5,6 @@ from exceptions import ApiErrorException
 v = VesselFinderApi(userkey='WS-22E9F000-28BD82', errormode=False, save_last_info=True)
 
 try:
-    print(v.distance(_from="1.24703,51.94967", _to="29.68018,40.96205"))
+    print(v.distance(_from="1.24703,51.94967", _to="28.68018,40.96205"))
 except ApiErrorException as e:
     print(e)
